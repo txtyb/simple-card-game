a95d67cc66178d8c0cb4e64e0f6b51deb92543b2
txtyb <txtybbill@163.com>
2018-06-07 14:36:30 +0800

    Complete roundstart(), consrtacted half of gamecheck()

--- main.py
+++ main.py
@@ -2,5 +2,23 @@ import time,random
 
 cardrange = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
 
-newcard = random.choice(cardrange)
 #print(newcard)
+cards = list()
+
+def roundstart():
+    newcard = random.choice(cardrange)
+    cards.append(newcard)
+    print(cards)
+
+def gamechecker():
+    s_card_position=-1
+    for s_card in cards:
+        print('s_card is:%s'%s_card)
+        s_card_position+=1
+        print('s_position is:%s'%s_card_position)
+        e_card_position=cards[s_card_position:].index(s_card)
+        print(s_card_position,e_card_position)
+
+for i in range(1,8):
+    roundstart()
+    gamechecker()
