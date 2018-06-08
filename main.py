8bac5757581688bdc4e0bea80a0e8e260cb402e4
txtyb <txtybbill@163.com>
2018-06-08 14:06:55 +0800

    Completed the main part, the next step is the interface part.

--- main.py
+++ main.py
@@ -4,35 +4,65 @@ score=0
 cardrange = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
 
 #print(newcard)
+dcards = list()
 cards = list()
 
 def roundstart():
+    '''回合开始发卡片'''
+    dcards.clear()
+    for i in range(len(cards)-1,-1,-1):
+        dcards.append(cards[i])
+        #初始化dcards，以从cards传回更改
     newcard = random.choice(cardrange)
-    cards.append(newcard)
+    dcards.append(newcard)
+    cards.clear()
+    #清空卡片避免造成叠加重复
+    #print('dcards= ',dcards)
+    for i in range(len(dcards)-1,-1,-1):
+        cards.append(dcards[i])
+        #将卡片倒序排列
     print('回合开始时的卡片：',cards)
 
 def gamechecker():
-    for s_card in cards:
-    # print('s_card is:%s'%s_card)
-    # s_card_position+=1
-    # print('s_position is:%s'%s_card_position)
-        e_card_position = cards.index(s_card)
+    '''是否得分判断'''
+    s_card_position=0
+    global e_card_position
+    s_card=cards[s_card_position]
+    #for s_card in cards:
+        #print('s_card is:%s'%s_card)
+        #s_card_position+=1
+        #print('s_position is:%s'%s_card_position)
+    try:
+        e_card_position = cards[s_card_position+1:].index(s_card)
+    except ValueError:
+        print('没有得分点')
+        #pass
+    else:
+        print('发现得分点')
+        e_card_position+=1
+        #重要！切分s_card_position时产生了新的列表，这使得e_card_position少了1
         print('e_card_position=%s'%e_card_position)
         # print(s_card_position,e_card_position)
-        if e_card_position!=0:
-            cards_num=e_card_position-0
-            scount(cards_num)
-            rmcards=cards[:e_card_position]
-            for percard in rmcards:
-                cards.remove(percard)
-                #清除得分的卡片
-                print('清除后：',cards)
+        cards_num=e_card_position-0
+        scount(cards_num)
+        rm()
+        return
+
+def rm():
+    rmcards=cards[:e_card_position+1]
+    print('rmcards= ',rmcards)
+    for percard in rmcards:
+        cards.remove(percard)
+        #清除得分的卡片
+        print('清除后：',cards)
 
 def scount(num):
+    '''得分计算部分，尚未完成'''
     global score
     score+=num*5
 
-for i in range(1,8):
+while True:
     roundstart()
     gamechecker()
     print('score=%s'%score)
+    input('---------------')
--- t.py
+++ t.py
@@ -1,3 +1,18 @@
-a=['a','b','c','d','e']
-num=a.index('o')
-print(num)
+a=['1','2','3','4','5']
+b=list()
+
+for i in range(len(a)-1,-1,-1):
+    b.append(a[i])
+print(b)
+print('---------------')
+
+try:
+    p=a.index(input('输入要查找的值：'))
+except ValueError:
+    print('没有找到')
+else:
+    print('位于%s'%p)
+print('--------------')
+
+print('a= ',a)
+print(a[:3])
